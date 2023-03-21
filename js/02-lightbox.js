384972caa8c2328fba6d7c1cae2bfa94bbb5e782
VVervolk <vervolk1234@gmail.com>
2023-03-21 20:15:03 +0200

    2 task 3 options complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    item =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href=${item.original}>
+    <img class="gallery__image" src=${item.preview} alt=${item.description} />
+  </a>
+</li>`
+  )
+  .join('');
+
+gallery.innerHTML = markup;
+
+gallery.addEventListener('click', openBigImage);
+
+function openBigImage(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  let bigImageModal = new SimpleLightbox('.gallery a', {
+    captionSelector() {
+      console.log(evt);
+    },
+  });
+
+  bigImageModal.on('show.simplelightbox', function () {
+    // Do something…
+  });
+}
